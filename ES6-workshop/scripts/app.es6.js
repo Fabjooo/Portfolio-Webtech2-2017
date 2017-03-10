98690d07b70f17c624f2cea50e58c4f57102d513
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-03-10 16:36:06 +0100

    template strings in plaats van concatenering

--- ES6-workshop/scripts/app.es6.js
+++ ES6-workshop/scripts/app.es6.js
@@ -1,39 +1,47 @@
-function NoteApp() {
-	this.buttonAddNote = document.getElementById('btnAddNote');
-	this.notesContainer = document.querySelector(".notes");
-	this.noteInput = document.querySelector("#txtAddNote");
+class NotesApp {
+    constructor() {
+        this.buttonAddNote = document.getElementById('btnAddNote');
+        this.notesContainer = document.querySelector(".notes");
+        this.noteInput = document.querySelector("#txtAddNote");
 
-	this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
+        this.buttonAddNote.addEventListener("click", this.addNote.bind(this));
+    }
+
+    addNote(event) {
+        let newNote = document.createElement("div");
+        newNote.setAttribute("class", "card");
+        newNote.innerHTML = `<p> ${this.noteInput.value} </p>`;
+
+        let notelink = document.createElement("a");
+        notelink.setAttribute("class", "card-remove");
+        notelink.innerHTML = "remove";
+        notelink.setAttribute("href", "#");
+        notelink.addEventListener("click", this.removeNote);
+
+        newNote.appendChild(notelink);
+
+        this.notesContainer.appendChild(newNote);
+        this.resetNotesForm();
+        
+        event.preventDefault();
+    }
+
+    removeNote(event){
+         console.log("removing note");
+ 
+         var elementToDelete = event.target.parentElement;
+         this.notesContainer.removeChild(elementToDelete);
+         console.log(event);
+         event.preventDefault();
+     }
+
+    resetNotesForm() {
+        console.log("resetting the notes form");
+        
+        this.noteInput.value ="";
+        this.noteInput.focus();
+    }
+
 }
 
-NoteApp.prototype.addNote = function() {
-	let newNote = document.createElement("div");
-	newNote.setAttribute("class", "card");
-	newNote.innerHTML = "<p>" + this.noteInput.value + "</p>";
-
-	let notelink = document.createElement("a");
-	notelink.setAttribute("class", "card-remove");
-	notelink.innerHTML = "remove";
-	notelink.setAttribute("href", "#");
-	notelink.addEventListener("click", this.removeNote);
-
-	newNote.appendChild(notelink);
-
-	this.notesContainer.appendChild(newNote);
-	this.resetForm();
-}
-
-NoteApp.prototype.removeNote = function() {
-	
-    let noteToRemove = e.target.parentElement;
-    this.notesContainer.removeChild(noteToRemove);
-    
-    e.preventDefault();
-}
-
-NoteApp.prototype.resetForm = function() {
-	this.noteInput.value ="";
-	this.noteInput.focus();
-}
-
-let myApp = new NoteApp();
+let myApp = new NotesApp();
